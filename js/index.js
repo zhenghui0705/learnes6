518e237a590c47437652d353b5dff71bfb7f7e95
zhenghui0705 <648338777qq.com>
2018-01-16 14:01:59 +0800

    从入门到放弃

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*
+模板字符串
+es6语法会自动替换字符串中的变量
+let name = '小明'
+let age = 22
+let message = `${name},你今年${age},了吧！`
+console.log(message);
+
+es5语法
+var name = '张三'
+var age  =  23
+var message = name + '你今年' + age + '了吧！'
+console.log(message)
+*/
